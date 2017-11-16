e3c64c92045d98410e94db9055a65f24151b4c97
cucy <292016176@qq.com>
2017-11-16 16:58:44 +0800

    详情页

--- apps/products/models.py
+++ apps/products/models.py
@@ -3,16 +3,15 @@ from django.db import models
 
 class Product(models.Model):
     """商品模型"""
-    title = models.CharField(max_length=120, name='商品名')
-    description = models.TextField(name='商品描述')
-    price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99, name="商品价格")
+    title = models.CharField(max_length=120, verbose_name="商品名")
+    description = models.TextField(verbose_name='商品描述')
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99, verbose_name="商品价格")
 
-    #
-    # def __str__(self):
-    #     return self.title
-    #
-    # def __repr__(self):
-    #     return self.title
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return self.title
 
     class Meta:
         verbose_name = '商品模型'
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Product
 
@@ -15,3 +15,20 @@ def product_list_view(request):
     queryset = Product.objects.all()
     context = {'object_list': queryset}
     return render(request, 'products/list.html', context)
+
+
+class ProductDetailView(DetailView):
+    """商品详情视图"""
+    queryset = Product.objects.all()
+    template_name = 'products/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
+        print(context)
+        return context
+
+
+def product_detail_view(request, pk=None, *args, **kwargs):
+    instance = get_object_or_404(Product, pk=pk)
+    context = {'object': instance}
+    return render(request, 'products/detail.html', context)
