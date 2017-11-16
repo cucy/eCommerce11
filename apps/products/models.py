0dc921414fae54b2407546e422c423fee283f64a
cucy <292016176@qq.com>
2017-11-16 17:25:06 +0800

    图片上传

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,4 +1,25 @@
+import os
+import random
+
 from django.db import models
+
+
+def get_filename_ext(filepath):
+    """获取文件名和扩展名函数"""
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    """生成新的文件扩展名"""
+    new_filename = random.randint(1, 2345623456)
+    name, ext = get_filename_ext(filename)
+    final_filename = "{new_filename}{ext}".format(new_filename=new_filename, ext=ext)
+    return "products/{new_filename}/{final_filename}".format(
+        new_filename=new_filename,
+        final_filename=final_filename
+    )
 
 
 class Product(models.Model):
@@ -6,6 +27,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120, verbose_name="商品名")
     description = models.TextField(verbose_name='商品描述')
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99, verbose_name="商品价格")
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
     def __str__(self):
         return self.title
