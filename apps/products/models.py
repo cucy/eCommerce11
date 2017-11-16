a8f49f8115d8567c405a8a5b996980bd44272d6f
cucy <292016176@qq.com>
2017-11-16 14:32:30 +0800

    admin后台

--- apps/products/models.py
+++ apps/products/models.py
@@ -6,3 +6,14 @@ class Product(models.Model):
     title = models.CharField(max_length=120, name='商品名')
     description = models.TextField(name='商品描述')
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99, name="商品价格")
+
+    #
+    # def __str__(self):
+    #     return self.title
+    #
+    # def __repr__(self):
+    #     return self.title
+
+    class Meta:
+        verbose_name = '商品模型'
+        verbose_name_plural = verbose_name
