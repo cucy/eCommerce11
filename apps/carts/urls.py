e4070a06432b7b0447b3449fba042cb3a82aefd6
cucy <292016176@qq.com>
2017-11-17 18:43:49 +0800

    购物车  Add to Cart Form

--- apps/carts/urls.py
+++ apps/carts/urls.py
@@ -3,6 +3,6 @@ from carts.views import cart_home, cart_update
 
 urlpatterns = [
 
-    url(r'^$', cart_home, name='cart'),
+    url(r'^$', cart_home, name='home'),
     url(r'^update/$', cart_update, name='update'),
 ]
