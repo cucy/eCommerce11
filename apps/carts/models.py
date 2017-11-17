1a91b0c958812d859799a2f8bc716a19ca853cf8
cucy <292016176@qq.com>
2017-11-17 18:06:38 +0800

    购物车 Cart Model Manager

--- apps/carts/models.py
+++ apps/carts/models.py
@@ -6,6 +6,15 @@ from products.models import Product
 User = settings.AUTH_USER_MODEL
 
 
+class CartManager(models.Manager):
+    def new(self, user=None):
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated():
+                user_obj = user
+        return self.model.objects.create(user=user_obj)
+
+
 class Cart(models.Model):
     """购物车"""
     user = models.ForeignKey(User, null=True, blank=True, verbose_name='用户名')
@@ -13,6 +22,8 @@ class Cart(models.Model):
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2, verbose_name='总价')
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = CartManager()
 
     def __str__(self):
         return str(self.id)
--- apps/carts/views.py
+++ apps/carts/views.py
@@ -3,35 +3,16 @@ from django.shortcuts import render
 from .models import Cart
 
 
-def cart_create(user=None):
-    cart_obj = Cart.objects.create(user=None)
-    print("New Cart created")
-    return cart_obj
-
-
 def cart_home(request):
-    """
-        print('cat====================== ')
-        print(request.session) # on the request
-        print(dir(request.session))
-        request.session.set_expiry(300) # 5 minutes
-        key = request.session.session_key
-        print(key)
-        print('cat====================== ')
-        request.session["card_id"] = 12
-        request.session['user'] = request.user.username
-    """
-    request.session['cart_id'] = "12"
     cart_id = request.session.get("cart_id", None)
-    if cart_id is None:
-        cart_obj = cart_create()
-        request.session['cart_id'] = cart_obj.id
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
+        print('Cart ID exists')
+        cart_obj = qs.first()
+        if request.user.is_authenticated() and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
     else:
-        qs = Cart.objects.filter(id=cart_id)
-        if qs.count() == 1:
-            print('Cart ID exists')
-            cart_obj = qs.first()
-        else:
-            cart_obj = cart_create()
-            request.session['cart_id'] = cart_obj.id
+        cart_obj = Cart.objects.new(user=request.user)
+        request.session['cart_id'] = cart_obj.id
     return render(request, "carts/home.html", {})
