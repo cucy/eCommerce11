c80b1a12c93afac421ba7fa4e656d932ecae251f
cucy <292016176@qq.com>
2017-11-17 17:47:32 +0800

    购物车 cart模型

--- apps/carts/models.py
+++ apps/carts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from products.models import Product
+
+User = settings.AUTH_USER_MODEL
+
+
+class Cart(models.Model):
+    """购物车"""
+    user = models.ForeignKey(User, null=True, blank=True, verbose_name='用户名')
+    products = models.ManyToManyField(Product, blank=True)
+    total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2, verbose_name='总价')
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "购物车"
+        verbose_name_plural = verbose_name
--- apps/carts/views.py
+++ apps/carts/views.py
@@ -13,7 +13,13 @@ def cart_home(request):
         key = request.session.session_key
         print(key)
         print('cat====================== ')
+        request.session["card_id"] = 12
+        request.session['user'] = request.user.username
     """
-    request.session["card_id"] = 12
-    request.session['user'] = request.user.username
+    cart_id = request.session.get("cart_id", None)
+    if cart_id is None:  # and isinstance(cart_id, int):
+        print('create new cart')
+        request.session['cart_id'] = 12
+    else:
+        print('Cart ID exists')
     return render(request, 'carts/home.html')
