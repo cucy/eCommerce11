c64bb1369610f5dc20a0d18492157325c207c061
cucy <292016176@qq.com>
2017-11-17 17:26:06 +0800

    外键查询

--- apps/products/models.py
+++ apps/products/models.py
@@ -40,7 +40,8 @@ class ProductQuerySet(models.query.QuerySet):
         lookups = (
                 Q(title__icontains=query) |
                 Q(description__icontains=query) |
-                Q(price__icontains=query)
+                Q(price__icontains=query) |
+                Q(tag__title__icontains=query)  # 外键查询
         )
         return self.filter(lookups).distinct()
 
