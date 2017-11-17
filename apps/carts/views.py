8426b7a736de77a98ce93b812e1ad6acf70e67ec
cucy <292016176@qq.com>
2017-11-17 17:38:33 +0800

    session

--- apps/carts/views.py
+++ apps/carts/views.py
@@ -5,4 +5,15 @@ from django.shortcuts import render
 
 
 def cart_home(request):
+    """
+        print('cat====================== ')
+        print(request.session) # on the request
+        print(dir(request.session))
+        request.session.set_expiry(300) # 5 minutes
+        key = request.session.session_key
+        print(key)
+        print('cat====================== ')
+    """
+    request.session["card_id"] = 12
+    request.session['user'] = request.user.username
     return render(request, 'carts/home.html')
