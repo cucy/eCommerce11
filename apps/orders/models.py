3e810dd24a62bbef6ce04c7f910eeb6266a5e724
cucy <292016176@qq.com>
2017-11-18 14:23:59 +0800

    Associate Addresses to Order 联系地址订单

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -5,6 +5,7 @@ from django.db.models.signals import pre_save, post_save
 from billing.models import BillingProfile
 from carts.models import Cart
 from eCommerce.utils import unique_order_id_generator
+from addresses.models import Address
 
 ORDER_STATUS_CHOICES = (
     ('created', 'Created'),
@@ -37,9 +38,8 @@ class OrderManager(models.Manager):
 class Order(models.Model):
     billing_profile = models.ForeignKey(BillingProfile, null=True, blank=True)
     order_id = models.CharField(max_length=120, blank=True)  # AB31DE3
-    # billing_profile = ?
-    # shipping_address
-    # billing_address
+    shipping_address = models.ForeignKey(Address, related_name="shipping_address", null=True, blank=True)
+    billing_address = models.ForeignKey(Address, related_name="billing_address", null=True, blank=True)
     cart = models.ForeignKey(Cart)
     status = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
     shipping_total = models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
