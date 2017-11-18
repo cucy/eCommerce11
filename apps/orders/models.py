1a9f0defd6807411a8f3cb87be8964c4b90463d2
cucy <292016176@qq.com>
2017-11-18 09:57:27 +0800

    计算订单价格  Calculate the Order Total

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from carts.models import Cart
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 
 from eCommerce.utils import unique_order_id_generator
 
@@ -26,6 +26,15 @@ class Order(models.Model):
     def __str__(self):
         return self.order_id
 
+    def update_total(self):
+        """计算订单总价格"""
+        cart_total = self.cart.total
+        shipping_total = self.shipping_total
+        new_total = cart_total + shipping_total
+        self.total = new_total
+        self.save()
+        return new_total
+
 
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
@@ -33,5 +42,27 @@ def pre_save_create_order_id(sender, instance, *args, **kwargs):
 
 
 pre_save.connect(pre_save_create_order_id, sender=Order)
-# generate the order id?
-# generate the order total?
+
+
+def post_save_cart_total(sender, instance, created, *args, **kwargs):
+    if not created:
+        cart_obj = instance
+        cart_total = cart_obj.total
+        cart_id = cart_obj.id
+        qs = Order.objects.filter(cart__id=cart_id)
+        if qs.count() == 1:
+            order_obj = qs.first()
+            order_obj.update_total()
+
+
+post_save.connect(post_save_cart_total, sender=Cart)
+
+
+def post_save_order(sender, instance, created, *args, **kwargs):
+    print('running')
+    if created:
+        print("Updating... first")
+        instance.update_total()
+
+
+post_save.connect(post_save_order, sender=Order)
