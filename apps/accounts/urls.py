6d5b99f0e43b36a3519cccd312149cc6aa30622b
cucy <292016176@qq.com>
2017-11-22 14:53:23 +0800

    User Product History View

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -5,11 +5,13 @@ from .views import (
     AccountEmailActivateView,
     UserDetailUpdateView,
 )
+from products.views import UserProductHistoryView
 
 urlpatterns = [
 
     url(r'^$', AccountHomeView.as_view(), name='home'),
     url(r'^details/$', UserDetailUpdateView.as_view(), name='user-update'),
+    url(r'history/products/$', UserProductHistoryView.as_view(), name='user-product-history'),
     url(r'^email/confirm/(?P<key>[0-9A-Za-z]+)/$',
         AccountEmailActivateView.as_view(),
         name='email-activate'),
