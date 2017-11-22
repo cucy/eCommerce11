7fa08103f3008ea3aa5cddca0103521bd6c5c3d8
cucy <292016176@qq.com>
2017-11-22 15:29:28 +0800

    16 - Orders & Order Detail

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     url(r'^billing/payment-method/create/$', payment_method_createview, name='billing-payment-method-endpoint'),
     url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^bootstrap/$', TemplateView.as_view(template_name='example.html')),
+    url(r'^orders/', include("orders.urls", namespace='orders')),
     url(r'^products/', include("products.urls", namespace='products')),
     url(r'^search/', include("search.urls", namespace='search')),
 
